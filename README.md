cad13e02436bc378215e1e453a1c30f0337a2c19
padanyi-gulyasg <padanyi-gulyasg@si.net>
2020-09-01 15:50:13 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# oitm_openlayers
+# OITM - OpenLayers
+
+1. Függőségek telepítése:
+
+```bash
+npm install
+```
+
+2. Kód futtatása:
+
+```bash
+npm run start
+```
+
+3. Böngészőben:
+
+```bash
+http://localhost:1234
+```
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import 'ol/ol.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 0
+  })
+});
+
+setTimeout(() => {
+  map.getView().fit([2037098, 6045281, 2044273, 6049066], { duration: 2000 })
+}, 1000)
